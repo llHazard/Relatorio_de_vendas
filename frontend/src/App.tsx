0d27c989c556b31feb36c63b004067b84257d403
Felipe Rodrigues <feliperodriguesdeolliveira@gmail.com>
2022-11-24 19:39:48 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,12 @@
+import NotificationButton from "./components/NotificationButton"
+
 function App() {
   return(
-    <h1>Olá mundo!</h1>
+    <>
+    <h1>Ola,Mundo!</h1>
+    <NotificationButton/>
+    <NotificationButton/>
+    </>
   )
 }
 
