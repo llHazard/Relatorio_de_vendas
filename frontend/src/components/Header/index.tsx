bf79469ef53b6aa39743e0b75509806163fa12c0
Felipe Rodrigues <feliperodriguesdeolliveira@gmail.com>
2022-11-24 20:39:34 -0300

    Datepicker

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -7,7 +7,7 @@ function Header() {
         <header>
             <div className="dsmeta-logo-container">
                 <img src={logo} alt="DSMeta" />
-                <h1>DSMeta</h1>
+                <h1>Controle de vendas</h1>
                 <p>
                     Desenvolvido por
                     <a href="https://www.instagram.com/devsuperior.ig">@feliperodriguesr_</a>
